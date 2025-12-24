ce16241d26401b2347771db6235c3e52cf48841b
jraranas <jbaranas@up.edu.ph>
2025-12-24 18:03:01 +0800

    refactor(py): changed  if-else statements to match-case

--- implementation1/spritemap.py
+++ implementation1/spritemap.py
@@ -182,118 +182,134 @@ def init_sprites(cls): # type: ignore
 
 @staticmethod
 def get_player_sprite(cls: SpriteMap, player: int, direction: str) -> list[SpriteCoords]:
-    if player == 1:
-        if direction == "NORTH":
-            return [cls.P1_NORTH, cls.P1_WALK_NORTH_1, cls.P1_NORTH, cls.P1_WALK_NORTH_2]
-        elif direction == "SOUTH":
-            return [cls.P1_SOUTH, cls.P1_WALK_SOUTH_1, cls.P1_SOUTH, cls.P1_WALK_SOUTH_2]
-        elif direction == "EAST":
-            return [cls.P1_EAST, cls.P1_WALK_EAST_1, cls.P1_EAST, cls.P1_WALK_EAST_2]
-        else: # "WEST"
-            return [cls.P1_WEST, cls.P1_WALK_WEST_1, cls.P1_WEST, cls.P1_WALK_WEST_2]
-
-    elif player == 2:
-        if direction == "NORTH":
-            return [cls.P2_NORTH, cls.P2_WALK_NORTH_1, cls.P2_NORTH, cls.P2_WALK_NORTH_2]
-        elif direction == "SOUTH":
-            return [cls.P2_SOUTH, cls.P2_WALK_SOUTH_1, cls.P2_SOUTH, cls.P2_WALK_SOUTH_2]
-        elif direction == "EAST":
-            return [cls.P2_EAST, cls.P2_WALK_EAST_1, cls.P2_EAST, cls.P2_WALK_EAST_2]
-        else: # "WEST"
-            return [cls.P2_WEST, cls.P2_WALK_WEST_1, cls.P2_WEST, cls.P2_WALK_WEST_2]
-        
-    elif player == 3:
-        if direction == "NORTH":
-            return [cls.P3_NORTH, cls.P3_WALK_NORTH_1, cls.P3_NORTH, cls.P3_WALK_NORTH_2]
-        elif direction == "SOUTH":
-            return [cls.P3_SOUTH, cls.P3_WALK_SOUTH_1, cls.P3_SOUTH, cls.P3_WALK_SOUTH_2]
-        elif direction == "EAST":
-            return [cls.P3_EAST, cls.P3_WALK_EAST_1, cls.P3_EAST, cls.P3_WALK_EAST_2]
-        else: # "WEST"
-            return [cls.P3_WEST, cls.P3_WALK_WEST_1, cls.P3_WEST, cls.P3_WALK_WEST_2]
-    
-    else: # player == 4
-        if direction == "NORTH":
-            return [cls.P4_NORTH, cls.P4_WALK_NORTH_1, cls.P4_NORTH, cls.P4_WALK_NORTH_2]
-        elif direction == "SOUTH":
-            return [cls.P4_SOUTH, cls.P4_WALK_SOUTH_1, cls.P4_SOUTH, cls.P4_WALK_SOUTH_2]
-        elif direction == "EAST":
-            return [cls.P4_EAST, cls.P4_WALK_EAST_1, cls.P4_EAST, cls.P4_WALK_EAST_2]
-        else: # "WEST"
-            return [cls.P4_WEST, cls.P4_WALK_WEST_1, cls.P4_WEST, cls.P4_WALK_WEST_2]
+    match player:
+        case 1:
+            match direction:
+                case "NORTH":
+                    return [SpriteMap.P1_NORTH, SpriteMap.P1_WALK_NORTH_1, SpriteMap.P1_NORTH, SpriteMap.P1_WALK_NORTH_2]
+                case "SOUTH":
+                    return [SpriteMap.P1_SOUTH, SpriteMap.P1_WALK_SOUTH_1, SpriteMap.P1_SOUTH, SpriteMap.P1_WALK_SOUTH_2]
+                case "EAST":
+                    return [SpriteMap.P1_EAST, SpriteMap.P1_WALK_EAST_1, SpriteMap.P1_EAST, SpriteMap.P1_WALK_EAST_2]
+                case _:  # "WEST"
+                    return [SpriteMap.P1_WEST, SpriteMap.P1_WALK_WEST_1, SpriteMap.P1_WEST, SpriteMap.P1_WALK_WEST_2]
+        case 2:
+            match direction:
+                case "NORTH":
+                    return [SpriteMap.P2_NORTH, SpriteMap.P2_WALK_NORTH_1, SpriteMap.P2_NORTH, SpriteMap.P2_WALK_NORTH_2]
+                case "SOUTH":
+                    return [SpriteMap.P2_SOUTH, SpriteMap.P2_WALK_SOUTH_1, SpriteMap.P2_SOUTH, SpriteMap.P2_WALK_SOUTH_2]
+                case "EAST":
+                    return [SpriteMap.P2_EAST, SpriteMap.P2_WALK_EAST_1, SpriteMap.P2_EAST, SpriteMap.P2_WALK_EAST_2]
+                case _:  # "WEST"
+                    return [SpriteMap.P2_WEST, SpriteMap.P2_WALK_WEST_1, SpriteMap.P2_WEST, SpriteMap.P2_WALK_WEST_2]
+        case 3:
+            match direction:
+                case "NORTH":
+                    return [SpriteMap.P3_NORTH, SpriteMap.P3_WALK_NORTH_1, SpriteMap.P3_NORTH, SpriteMap.P3_WALK_NORTH_2]
+                case "SOUTH":
+                    return [SpriteMap.P3_SOUTH, SpriteMap.P3_WALK_SOUTH_1, SpriteMap.P3_SOUTH, SpriteMap.P3_WALK_SOUTH_2]
+                case "EAST":
+                    return [SpriteMap.P3_EAST, SpriteMap.P3_WALK_EAST_1, SpriteMap.P3_EAST, SpriteMap.P3_WALK_EAST_2]
+                case _:  # "WEST"
+                    return [SpriteMap.P3_WEST, SpriteMap.P3_WALK_WEST_1, SpriteMap.P3_WEST, SpriteMap.P3_WALK_WEST_2]
+        case _:  # player == 4
+            match direction:
+                case "NORTH":
+                    return [SpriteMap.P4_NORTH, SpriteMap.P4_WALK_NORTH_1, SpriteMap.P4_NORTH, SpriteMap.P4_WALK_NORTH_2]
+                case "SOUTH":
+                    return [SpriteMap.P4_SOUTH, SpriteMap.P4_WALK_SOUTH_1, SpriteMap.P4_SOUTH, SpriteMap.P4_WALK_SOUTH_2]
+                case "EAST":
+                    return [SpriteMap.P4_EAST, SpriteMap.P4_WALK_EAST_1, SpriteMap.P4_EAST, SpriteMap.P4_WALK_EAST_2]
+                case _:  # "WEST"
+                    return [SpriteMap.P4_WEST, SpriteMap.P4_WALK_WEST_1, SpriteMap.P4_WEST, SpriteMap.P4_WALK_WEST_2]
 
 @staticmethod
 def get_player_idle(cls: SpriteMap, player: int, direction: str) -> SpriteCoords:
-    if player == 1:
-        if direction == "NORTH":
-            return cls.P1_NORTH
-        elif direction == "SOUTH":
-            return cls.P1_SOUTH
-        elif direction == "EAST":
-            return cls.P1_EAST
-        else: # "WEST"
-            return cls.P1_WEST
-    elif player == 2:
-        if direction == "NORTH":
-            return cls.P2_NORTH
-        elif direction == "SOUTH":
-            return cls.P2_SOUTH
-        elif direction == "EAST":
-            return cls.P2_EAST
-        else: # "WEST"
-            return cls.P2_WEST    
-    elif player == 3:
-        if direction == "NORTH":
-            return cls.P3_NORTH
-        elif direction == "SOUTH":
-            return cls.P3_SOUTH
-        elif direction == "EAST":
-            return cls.P3_EAST
-        else: # "WEST"
-            return cls.P3_WEST    
-    else: # player == 4
-        if direction == "NORTH":
-            return cls.P4_NORTH
-        elif direction == "SOUTH":
-            return cls.P4_SOUTH
-        elif direction == "EAST":
-            return cls.P4_EAST
-        else: # "WEST"
-            return cls.P4_WEST
+    match player:
+        case 1:
+            match direction:
+                case "NORTH":
+                    return SpriteMap.P1_NORTH
+                case "SOUTH":
+                    return SpriteMap.P1_SOUTH
+                case "EAST":
+                    return SpriteMap.P1_EAST
+                case _:  # "WEST"
+                    return SpriteMap.P1_WEST
+        case 2:
+            match direction:
+                case "NORTH":
+                    return SpriteMap.P2_NORTH
+                case "SOUTH":
+                    return SpriteMap.P2_SOUTH
+                case "EAST":
+                    return SpriteMap.P2_EAST
+                case _:  # "WEST"
+                    return SpriteMap.P2_WEST    
+        case 3:
+            match direction:
+                case "NORTH":
+                    return SpriteMap.P3_NORTH
+                case "SOUTH":
+                    return SpriteMap.P3_SOUTH
+                case "EAST":
+                    return SpriteMap.P3_EAST
+                case _:  # "WEST"
+                    return SpriteMap.P3_WEST    
+        case _:  # player == 4
+            match direction:
+                case "NORTH":
+                    return SpriteMap.P4_NORTH
+                case "SOUTH":
+                    return SpriteMap.P4_SOUTH
+                case "EAST":
+                    return SpriteMap.P4_EAST
+                case _:  # "WEST"
+                    return SpriteMap.P4_WEST
 
 @staticmethod
 def get_player_death_sprite(cls: SpriteMap, player: int, frame: int) -> SpriteCoords: 
     frame = 0 # to be implemented later, hindi ko pa maisip/may ganaps pa kami hehe
-    death_sprite_loop: list[SpriteCoords] = []
+    death_sprite_loop: list[SpriteCoords] = [] # type: ignore 
 
-    if player == 1:
-        death_sprite_loop = [cls.P1_DEATH_1, cls.P1_DEATH_2, cls.P1_DEATH_3,cls.P1_DEATH_4, cls.P1_DEATH_5, cls. P1_DEATH_6]
-    elif player == 2:
-        death_sprite_loop =  [cls.P2_DEATH_1, cls.P2_DEATH_2, cls.P2_DEATH_3,cls.P2_DEATH_4, cls.P2_DEATH_5, cls. P2_DEATH_6]
-    elif player == 3:
-        death_sprite_loop = [cls.P3_DEATH_1, cls.P3_DEATH_2, cls.P3_DEATH_3,cls.P3_DEATH_4, cls.P3_DEATH_5, cls. P3_DEATH_6]
-    else: # player == 4
-        death_sprite_loop = [cls.P4_DEATH_1, cls.P4_DEATH_2, cls.P4_DEATH_3,cls.P4_DEATH_4, cls.P4_DEATH_5, cls. P4_DEATH_6]
+    match player:
+        case 1:
+            death_sprites = [SpriteMap.P1_DEATH_1, SpriteMap.P1_DEATH_2, SpriteMap.P1_DEATH_3,
+                            SpriteMap.P1_DEATH_4, SpriteMap.P1_DEATH_5, SpriteMap.P1_DEATH_6]
+        case 2:
+            death_sprites = [SpriteMap.P2_DEATH_1, SpriteMap.P2_DEATH_2, SpriteMap.P2_DEATH_3,
+                            SpriteMap.P2_DEATH_4, SpriteMap.P2_DEATH_5, SpriteMap.P2_DEATH_6]
+        case 3:
+            death_sprites= [SpriteMap.P3_DEATH_1, SpriteMap.P3_DEATH_2, SpriteMap.P3_DEATH_3,
+                            SpriteMap.P3_DEATH_4, SpriteMap.P3_DEATH_5, SpriteMap.P3_DEATH_6]
+        case _:  # player == 4
+            death_sprites = [SpriteMap.P4_DEATH_1, SpriteMap.P4_DEATH_2, SpriteMap.P4_DEATH_3,
+                            SpriteMap.P4_DEATH_4, SpriteMap.P4_DEATH_5, SpriteMap.P4_DEATH_6]
+    
+    return death_sprites[frame]
 
-    return death_sprite_loop[frame]
 @staticmethod
 def get_bomb_sprite(cls: SpriteMap, frame: int) -> SpriteCoords: 
     frame = frame % 3 # needed in order to cycle the 3 animations
-    if frame == 0:
-        return cls.BOMB_FRAME_1
-    elif frame == 1:
-        return cls.BOMB_FRAME_2
-    else: # frame == 2
-        return cls.BOMB_FRAME_3
+    match frame:
+        case 0:
+            return SpriteMap.BOMB_FRAME_1
+        case 1:
+            return SpriteMap.BOMB_FRAME_2
+        case _:
+            return SpriteMap.BOMB_FRAME_3
 
 @staticmethod
 def get_explosion_sprite(cls: SpriteMap, orientation: str, direction: str, frames: int  ) -> SpriteCoords: # 15 frames between animation change; 15 * 4 = 60 =2 seconds
-    if orientation == "CENTER":
-        return ...
-    elif orientation == "VERTICAL":
-        return ...
-    else: # orientation == "HORIZONTAL"
-        return ...
+    # if orientation == "CENTER":
+    #     return ...
+    # elif orientation == "VERTICAL":
+    #     return ...
+    # else: # orientation == "HORIZONTAL"
+    #     return ...
+    ...
+
 @staticmethod
 def get_soft_block_sprite(cls: SpriteMap, frame: int) -> SpriteCoords:
     ...
@@ -308,14 +324,15 @@ class Animation:
     
     def get_walk_frame(self) -> int:
         cycle = (self.frame // 5) % 4
-        if cycle == 0:
-            return 0
-        elif cycle == 1:
-            return 1
-        elif cycle == 2:
-            return 2
-        else:
-            return 1
+        match cycle:
+            case 0:
+                return 0
+            case 1:
+                return 1
+            case 2:
+                return 2
+            case _:  # cycle == 3
+                return 1
         
     def get_bomb_frame(self) -> int:
         return (self.frame // 10) % 3
