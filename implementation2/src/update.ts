91528852c8e8a2654dc18f35e645d518cb7f1b1f
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-12-29 00:50:05 +0800

    docs(ts): add comments in update.ts for potential implementation

--- implementation2/src/update.ts
+++ implementation2/src/update.ts
@@ -1,4 +1,11 @@
-import { Model } from "./model";
+import { Model,
+  Explosion,
+  Bomb,
+  Block,
+  Player,
+  Powerup,
+  // add others if needed
+} from "./model";
 import { Msg } from "./msg";
 import { Array, HashMap, pipe, Match } from "effect";
 
@@ -9,12 +16,39 @@ idea:
     which returns a new Struct of the updated entity
 */
 
+/*
+  NAMING CONVENTION FOR HELPER FUNCTIONS:
+    export const update<ENTITY> = ()
+*/
+
+// example:
+export const updateBomb = (ent: Bomb, dt: number): Bomb => {
+  return ent
+}
+
+// to consider: what if we make a separate `entity/` directory
+// containing the logic for all updating of entitites
+
 export const update = (msg: Msg, model: Model) =>
   Match.value(msg).pipe(
-    Match.tag("Canvas.MsgTick", () => model),
-    Match.tag("Canvas.MsgKeyDown", ({ key }): Model => {
+    Match.tag("Canvas.MsgTick", () => {
+      // pseudocode:
+      //   get all bombs in model: then do updateBomb for all
+      //   get all explosions in model: then do updateExpolsion for all
+      //   ...
+      //   get all <entity> in model: then do update<entity> for all
+      // ^ for all of these updates, update their entries in Model
+
+      // then, after allat... return the new Model:
       return model;
     }),
-    Match.tag("Canvas.MsgMouseDown", () => model),
+    Match.tag("Canvas.MsgKeyDown", ({ key }): Model => {
+      // Handle pressing of Keyboard
+      return model;
+    }),
+    Match.tag("Canvas.MsgMouseDown", () => {
+      // Handle pressing of Mouse
+      return model;
+    }),
     Match.exhaustive,
   );
