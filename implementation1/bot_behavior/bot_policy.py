f900887c3eb88858ecc80a1189e10d19f5f46906
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-12-28 23:29:20 +0800

    feat(py): update bot states and policies

--- implementation1/bot_behavior/bot_policy.py
+++ implementation1/bot_behavior/bot_policy.py
@@ -1,27 +1,32 @@
 
 import random
-from common_types import BombInfo, EntityType, ExplosionInfo, GridCoords, PlayerInfo, PowerupInfo, WorldInfo
+from common_types import BombInfo, ExplosionInfo, GridCoords, PlayerInfo, PowerupInfo, WorldInfo
 from bot_behavior.bot_types import BotMemoryInfo
-from bot_behavior.helpers.danger import get_manhattan
-from bot_behavior.helpers.pathfinding import get_shortest_path
+from bot_behavior.helpers.pathfinding import get_shortest_path, get_manhattan
 
 # ATTACK POLICIES
 
+
 class AttackPolicy1:
     """Attack only reachable enemies within a specific range"""
+
     def __init__(self, max_distance: int):
         self._max_distance = max_distance
 
+    def can_place_bomb(self) -> bool:
+        return True
+
     def get_goal(self, world: WorldInfo, bot: PlayerInfo) -> GridCoords | None:
         players = world.get_all_type(PlayerInfo)
+        opponents = [p for p in players if p.id != bot.id]
         candidates: list[tuple[GridCoords, int]] = []
 
-        for p in players:
-            if p.id == bot.id:
+        for opponent in opponents:
+            if opponent.id == bot.id:
                 continue
 
-            target_pos = (p.row, p.col)
-            
+            target_pos = (opponent.row, opponent.col)
+
             # Filter 1 - manhattan dist?
             dist = get_manhattan((bot.row, bot.col), target_pos)
             if dist > self._max_distance:
@@ -29,12 +34,12 @@ class AttackPolicy1:
 
             # Filter 2 - reachable? (can't traverse soft blocks)
             path = get_shortest_path(
-                world, 
-                (bot.row, bot.col), 
-                target_pos, 
-                traverse_soft_blocks=False
+                world,
+                (bot.row, bot.col),
+                target_pos,
+                ignore_soft_blocks=False
             )
-            
+
             if path:
                 candidates.append((target_pos, len(path)))
 
@@ -47,36 +52,52 @@ class AttackPolicy1:
     def get_path(self, world: WorldInfo, bot: PlayerInfo, memory: BotMemoryInfo) -> list[GridCoords]:
         if not memory.goal:
             return []
-        
+
         return get_shortest_path(
-            world, 
-            (bot.row, bot.col), 
-            memory.goal, 
-            traverse_soft_blocks=False
+            world,
+            (bot.row, bot.col),
+            memory.goal,
+            ignore_soft_blocks=False
         )
 
 
 class AttackPolicy2:
-    """Randomly target any enemy"""
+    """Randomly target any REACHABLE enemy"""
+
+    def can_place_bomb(self) -> bool:
+        return True
+
     def get_goal(self, world: WorldInfo, bot: PlayerInfo) -> GridCoords | None:
         players = world.get_all_type(PlayerInfo)
         opponents = [p for p in players if p.id != bot.id]
-        
+
         if not opponents:
             return None
-            
-        target = random.choice(opponents)
-        return (target.row, target.col)
+
+        random.shuffle(opponents)
+
+        for target in opponents:
+            target_pos = (target.row, target.col)
+            path = get_shortest_path(
+                world,
+                (bot.row, bot.col),
+                target_pos,
+                ignore_soft_blocks=True
+            )
+            if path:  # Only return if reachable
+                return target_pos
+
+        return None
 
     def get_path(self, world: WorldInfo, bot: PlayerInfo, memory: BotMemoryInfo) -> list[GridCoords]:
         if not memory.goal:
             return []
 
         return get_shortest_path(
-            world, 
-            (bot.row, bot.col), 
-            memory.goal, 
-            traverse_soft_blocks=True
+            world,
+            (bot.row, bot.col),
+            memory.goal,
+            ignore_soft_blocks=True
         )
 
 
@@ -84,37 +105,45 @@ class AttackPolicy2:
 
 class PowerupPolicy1:
     """Target the absolute closest powerup"""
+
+    def can_place_bomb(self) -> bool:
+        return True
+
     def get_goal(self, world: WorldInfo, bot: PlayerInfo) -> GridCoords | None:
         powerups = world.get_all_type(PowerupInfo)
         if not powerups:
             return None
-            
+
         start = (bot.row, bot.col)
-        
+
         sorted_powerups = sorted(
-            powerups, 
+            powerups,
             key=lambda p: get_manhattan(start, (p.row, p.col))
         )
-        
+
         target = sorted_powerups[0]
         return (target.row, target.col)
 
     def get_path(self, world: WorldInfo, bot: PlayerInfo, memory: BotMemoryInfo) -> list[GridCoords]:
         if not memory.goal:
             return []
-            
+
         return get_shortest_path(
-            world, 
-            (bot.row, bot.col), 
-            memory.goal, 
-            traverse_soft_blocks=True
+            world,
+            (bot.row, bot.col),
+            memory.goal,
+            ignore_soft_blocks=True
         )
 
 
 class PowerupPolicy2:
     """Target random reachable powerups nearby"""
+
     def __init__(self):
         self._search_radius = 4
+
+    def can_place_bomb(self) -> bool:
+        return False
 
     def get_goal(self, world: WorldInfo, bot: PlayerInfo) -> GridCoords | None:
         powerups = world.get_all_type(PowerupInfo)
@@ -123,44 +152,48 @@ class PowerupPolicy2:
 
         for p in powerups:
             p_pos = (p.row, p.col)
-            
+
             # Filter 1 - within 4 cells
             if get_manhattan(start, p_pos) > self._search_radius:
                 continue
-                
+
             # Filter 2 - reachable (No soft blocks)
-            path = get_shortest_path(world, start, p_pos, traverse_soft_blocks=False)
+            path = get_shortest_path(
+                world, start, p_pos, ignore_soft_blocks=False)
             if path:
                 candidates.append(p_pos)
-        
+
         if not candidates:
             return None
-            
+
         return random.choice(candidates)
 
     def get_path(self, world: WorldInfo, bot: PlayerInfo, memory: BotMemoryInfo) -> list[GridCoords]:
         if not memory.goal:
             return []
-            
-        return get_shortest_path(
-            world, 
-            (bot.row, bot.col), 
-            memory.goal, 
-            traverse_soft_blocks=False
-        )
 
+        return get_shortest_path(
+            world,
+            (bot.row, bot.col),
+            memory.goal,
+            ignore_soft_blocks=False
+        )
 
 
 # DANGER POLICIES
 
 class BombOnlyDangerPolicy:
     """Danger only from current bombs/explosions"""
+
     def is_in_danger(self, world: WorldInfo, bot: PlayerInfo, radius: int) -> bool:
+        overlap_pos = bot.get_overlapping_cells()
         bot_pos = (bot.row, bot.col)
-        
-        if self._check_cell(world, bot_pos):
+
+        danger_zones = self.get_all_danger_zones(world)
+
+        if any(p in danger_zones for p in overlap_pos):
             return True
-            
+
         if radius == 0:
             return False
 
@@ -168,30 +201,39 @@ class BombOnlyDangerPolicy:
             for c in range(bot.col - radius, bot.col + radius + 1):
                 if not world.in_bounds(r, c):
                     continue
-                
+
                 if get_manhattan(bot_pos, (r, c)) <= radius:
-                    if self._check_cell(world, (r, c)):
+                    if (r, c) in danger_zones:
                         return True
         return False
 
-    def _check_cell(self, world: WorldInfo, pos: GridCoords) -> bool:
-        r, c = pos
-        entity = world.get_entity_at(r, c)
-        if not entity:
-            return False
-        
-        return entity.entity_type in (EntityType.BOMB, EntityType.EXPLOSION)
+    def get_all_danger_zones(self, world: WorldInfo) -> set[GridCoords]:
+        danger_cells: set[GridCoords] = set()
+
+        # 1 - existing bombs and explosions
+        bombs = world.get_all_type(BombInfo)
+        for bomb in bombs:
+            danger_cells.add((bomb.row, bomb.col))
+
+        explosions = world.get_all_type(ExplosionInfo)
+        for exp in explosions:
+            danger_cells.add((exp.row, exp.col))
+
+        return danger_cells
 
 
 class ExplosionPredictionDangerPolicy:
-    """Danger from bombs, explosions, AND predicted blasts"""
-    def is_in_danger(self, world: WorldInfo, bot: PlayerInfo, radius: int) -> bool:
-        bot_pos = (bot.row, bot.col)
-        danger_zones = self._get_all_danger_zones(world)
+    """Danger ONLY from predicted blasts"""
 
-        if bot_pos in danger_zones:
+    def is_in_danger(self, world: WorldInfo, bot: PlayerInfo, radius: int) -> bool:
+        overlap_pos = bot.get_overlapping_cells()
+        bot_pos = (bot.row, bot.col)
+        danger_zones = self.get_all_danger_zones(world)
+
+        print(f"danger: {any(p in danger_zones for p in overlap_pos)}")
+        if any(p in danger_zones for p in overlap_pos):
             return True
-            
+
         if radius == 0:
             return False
 
@@ -199,24 +241,27 @@ class ExplosionPredictionDangerPolicy:
             for c in range(bot.col - radius, bot.col + radius + 1):
                 if not (0 <= r < world.rows and 0 <= c < world.cols):
                     continue
-                
+
                 if get_manhattan(bot_pos, (r, c)) <= radius:
                     if (r, c) in danger_zones:
                         return True
         return False
 
-    def _get_all_danger_zones(self, world: WorldInfo) -> set[GridCoords]:
+    def get_all_danger_zones(self, world: WorldInfo) -> set[GridCoords]:
         danger_cells: set[GridCoords] = set()
-        
+
         # 1 - existing explosions
         explosions = world.get_all_type(ExplosionInfo)
         for exp in explosions:
             danger_cells.add((exp.row, exp.col))
-            
+
         # 2 - bombs and their predicted blasts
         bombs = world.get_all_type(BombInfo)
         for b in bombs:
+            # dont include bomb
             danger_cells.add((b.row, b.col))
             danger_cells.update(b.get_affected_cells(world))
-            
+
+        print(danger_cells)
+
         return danger_cells
--- implementation1/bot_behavior/bot_state.py
+++ implementation1/bot_behavior/bot_state.py
@@ -1,65 +1,122 @@
-"""
-implementation for:
-- BaseState
-    - WanderState
-    - EscapeState
-    - GetPowerupState
-    - AttackState
-
-"""
-
-"""
-WanderState:
-- on_enter: pick random goal + path
-- decide_action: if next cell soft block and no bomb -> PLANT_BOMB else MOVE
-
-EscapeState:
-- on_enter: choose escape goal not dangerous
-- decide_action: move along path or switch to WANDER if reached
-
-AttackState:
-- on_enter: depends on attack policy
-- decide_action: follow path/update dynamic policies, plant bombs per rules
-
-GetPowerupState:
-- similar to WANDER but goal is powerup cell.
-"""
+from __future__ import annotations
+from common_types import EntityType, GridCoords, PlayerInfo, WorldInfo
+from bot_behavior.helpers.pathfinding import follow_path_action, get_random_floor_cell, get_reachable_safe_cell
+from .bot_types import Action, ActionInfo, BotState, PlayerAction, BotMemoryInfo
+from .bot_policy import get_shortest_path, get_manhattan
 
 
-from bot_behavior.bot_types import ActionInfo, BotMemoryInfo
-from common_types import PlayerInfo, WorldInfo
-
-# TEMPORARY; TO IMPLEMENT!
-class WanderState():
+class WanderState:
     def on_enter(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> None:
-        ...
-    def on_tick(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> None:
-        ...
-    def decide_action(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> ActionInfo | None:
-        ...
+        goal = get_random_floor_cell(world)
+        bot.set_goal(goal)
 
-class EscapeState():
+        path = get_shortest_path(
+            world, (entity.row, entity.col), goal, ignore_soft_blocks=True)
+        bot.set_path(path)
+
+    def on_tick(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> BotState | None:
+        if bot.goal and (entity.row, entity.col) == bot.goal:
+            self.on_enter(bot, world, entity)
+
+        if not bot.path and bot.goal != (entity.row, entity.col):
+            self.on_enter(bot, world, entity)
+
+        return None
+
+    def decide_action(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> ActionInfo | None:
+        return follow_path_action(bot, world, entity, allow_bombing=True)
+
+
+class EscapeState:
     def on_enter(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> None:
-        ...
-    def on_tick(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> None:
-        ...
+        self._left_danger = False
+        dangerous_cells = bot.config.danger_check_type.get_all_danger_zones(
+            world)
+        goal = get_reachable_safe_cell(world, entity, dangerous_cells)
+
+        if goal:
+            bot.set_goal(goal)
+            path = get_shortest_path(
+                world, (entity.row, entity.col), goal, ignore_soft_blocks=False)
+            bot.set_path(path)
+        else:
+            bot.set_goal(None)
+            bot.set_path([])
+
+    def on_tick(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> BotState | None:
+        overlap_pos = entity.get_overlapping_cells()
+        danger_zones = bot.config.danger_check_type.get_all_danger_zones(world)
+
+        if not bot.path:
+            self.on_enter(bot, world, entity)
+
+        in_danger = any(p in danger_zones for p in overlap_pos)
+        if not in_danger:
+            self._left_danger = True
+        else:
+            return None
+
+        if self._left_danger:
+            if any(p in danger_zones for p in bot.path):
+                self.on_enter(bot, world, entity)
+
+        # Success check
+        if bot.goal and (entity.row, entity.col) == bot.goal:
+            if not bot.is_strict_movement:
+                return WanderState()
+
+        # Failure check (Trapped)
+        if not bot.goal:
+            return WanderState()
+
+        return None
+
     def decide_action(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> ActionInfo | None:
-        ...
+        return follow_path_action(bot, world, entity, allow_bombing=False)
 
 
-class GetPowerupState():
+class GetPowerupState:
+    def __init__(self, target_goal: GridCoords):
+        self._target_goal = target_goal
+
     def on_enter(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> None:
-        ...
-    def on_tick(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> None:
-        ...
+        bot.set_goal(self._target_goal)
+        path = bot.config.powerup_policy.get_path(world, entity, bot)
+        bot.set_path(path)
+
+    def on_tick(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> BotState | None:
+        if (entity.row, entity.col) == bot.goal:
+            return WanderState()
+        return None
+
     def decide_action(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> ActionInfo | None:
-        ...
+        allow_bomb = bot.config.powerup_policy.can_place_bomb()
+        return follow_path_action(bot, world, entity, allow_bombing=allow_bomb)
 
 
-class AttackState():
+class AttackState:
+    def __init__(self, target_goal: GridCoords):
+        self._target_goal = target_goal
+
     def on_enter(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> None:
-        ...
-    def on_tick(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> None:
-        ...
+        bot.set_goal(self._target_goal)
+        path = bot.config.attack_policy.get_path(world, entity, bot)
+        bot.set_path(path)
+
+    def on_tick(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> BotState | None:
+        if (entity.row, entity.col) == bot.goal:
+            return WanderState()
+        return None
+
     def decide_action(self, bot: BotMemoryInfo, world: WorldInfo, entity: PlayerInfo) -> ActionInfo | None:
-        ...
+        # Check Attack Range Trigger
+        if bot.goal:
+            dist = get_manhattan((entity.row, entity.col), bot.goal)
+            if dist <= bot.config.attack_range_trigger:
+                curr = world.get_entity_at(entity.row, entity.col)
+                has_bomb = curr and curr.entity_type == EntityType.BOMB
+                if not has_bomb:
+                    return Action(PlayerAction.PLANT_BOMB)
+
+        # Move / Break Walls
+        return follow_path_action(bot, world, entity, allow_bombing=True)
