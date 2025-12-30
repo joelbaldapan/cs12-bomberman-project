fb3b0c9a823ed1c97af9073ca0d7782f539fcd90
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-12-30 21:42:58 +0800

    refactor(py): update bot type configs to be OCP compliant

--- implementation1/bot_behavior/bot.py
+++ implementation1/bot_behavior/bot.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
-from dataclasses import dataclass
 from bot_behavior.bot_policy import AttackPolicy1, AttackPolicy2, BombOnlyDangerPolicy, ExplosionPredictionDangerPolicy, PowerupPolicy1, PowerupPolicy2
-from common_types import BotConfigInfo, BotMemoryInfo, BotControllerInfo, BotState, ActionInfo, Action, DangerPolicy, PathfindingPolicy, PlayerAction, BombInfo, ExplosionInfo, PlayerInfo, WorldInfo, GridCoords, BotType
+from common_types import BotConfigInfo, BotMemoryInfo, BotControllerInfo, BotState, ActionInfo, Action, PlayerAction, BombInfo, ExplosionInfo, PlayerInfo, WorldInfo, GridCoords, BotType
 from .bot_state import AttackState, EscapeState, GetPowerupState, WanderState
 import random
 
@@ -17,6 +16,10 @@ class BotMemory():
         self._path: list[GridCoords] = []
         self._goal: GridCoords | None = None
         self._is_strict_movement: bool = False
+
+        # Perception _memory
+        self._last_bomb_coords: set[GridCoords] = set()
+        self._last_explosion_coords: set[GridCoords] = set()
 
     @property
     def config(self) -> BotConfigInfo:
@@ -50,6 +53,20 @@ class BotMemory():
     def set_goal(self, goal: GridCoords | None) -> None:
         self._goal = goal
 
+    @property
+    def last_bomb_coords(self) -> set[GridCoords]:
+        return self._last_bomb_coords
+    
+    def set_last_bomb_coords(self, coords: set[GridCoords]) -> None:
+        self._last_bomb_coords = coords
+
+    @property
+    def last_explosion_coords(self) -> set[GridCoords]:
+        return self._last_explosion_coords
+
+    def set_last_explosion_coords(self, coords: set[GridCoords]) -> None:
+        self._last_explosion_coords = coords
+
     def tick_reeval(self, dt: float) -> bool:
         """Advance the reevaluation timer. Return `True` if reevaluation should occur."""
         self._reeval_timer += dt
@@ -63,8 +80,8 @@ class BotMemory():
 
 class BotController:
     def __init__(self, bot_memory: BotMemoryInfo):
-        self.memory = bot_memory
-        self.current_state: BotState = WanderState()
+        self._memory = bot_memory
+        self._current_state: BotState = WanderState()
         self._initialized = False
         self._last_bomb_count = 0
         self._last_explosion_count = 0
@@ -72,7 +89,7 @@ class BotController:
     def update(self, dt: float, host_entity: PlayerInfo, world: WorldInfo) -> None:
         # Init state on first frame
         if not self._initialized:
-            self.current_state.on_enter(self.memory, world, host_entity)
+            self._current_state.on_enter(self._memory, world, host_entity)
             self._initialized = True
             # Track coordinates instead of IDs or Objects
             self._last_bomb_coords = {(b.row, b.col)
@@ -82,80 +99,81 @@ class BotController:
 
         should_force_reeval = False
 
-        # Update trackers for reevaluation
-        current_bombs = world.get_all_type(BombInfo)
-        current_bomb_coords = {(b.row, b.col) for b in current_bombs}
-        current_explosions = world.get_all_type(ExplosionInfo)
-        current_explosion_coords = {(e.row, e.col) for e in current_explosions}
+        current_bomb_coords = {
+            (b.row, b.col) for b in world.get_all_type(BombInfo)
+        }
+        current_explosion_coords = {
+            (e.row, e.col) for e in world.get_all_type(ExplosionInfo)
+        }
 
-        if len(self._last_explosion_coords - current_explosion_coords) > 0:
+        should_force_reeval = False
+
+        if self._memory.last_explosion_coords - current_explosion_coords:
             should_force_reeval = True
-        new_bomb_coords = current_bomb_coords - self._last_bomb_coords
-        if new_bomb_coords:
-            for (r, c) in new_bomb_coords:
-                dist = max(abs(host_entity.col - c), abs(host_entity.row - r))
-                if dist <= 5:
-                    print("debug: new bomb coords eval")
-                    should_force_reeval = True
-                    break
 
-        # Update trackers for next frame
-        self._last_bomb_coords = current_bomb_coords
-        self._last_explosion_coords = current_explosion_coords
+        new_bombs = current_bomb_coords - self._memory.last_bomb_coords
+        for (r, c) in new_bombs:
+            dist = max(abs(host_entity.col - c), abs(host_entity.row - r))
+            if dist <= 5:
+                should_force_reeval = True
+                break
+
+        self._memory.set_last_bomb_coords(current_bomb_coords)
+        self._memory.set_last_explosion_coords(current_explosion_coords)
 
 
-        timer_trigger = self.memory.tick_reeval(dt)
-        is_in_danger = self.memory.config.danger_check_type.is_in_danger(
-            world, host_entity, self.memory.config.danger_radius
+        timer_trigger = self._memory.tick_reeval(dt)
+        is_in_danger = self._memory.config.danger_check_type.is_in_danger(
+            world, host_entity, self._memory.config.danger_radius
         )
 
         # Check if danger!
         if is_in_danger:
-            if not isinstance(self.current_state, EscapeState):
+            if not isinstance(self._current_state, EscapeState):
                 print("Danger detected - Transitioning")
                 self.transition_to(EscapeState(), world, host_entity)
 
         if timer_trigger or should_force_reeval or is_in_danger:
             self._perform_global_reevaluation(world, host_entity)
 
-        if self.current_state:
-            next_state = self.current_state.on_tick(
-                self.memory, world, host_entity)
+        if self._current_state:
+            next_state = self._current_state.on_tick(
+                self._memory, world, host_entity)
             if next_state:
                 self.transition_to(next_state, world, host_entity)
 
     def decide_action(self, host_entity: PlayerInfo, world: WorldInfo) -> ActionInfo:
-        action = self.current_state.decide_action(
-            self.memory, world, host_entity)
+        action = self._current_state.decide_action(
+            self._memory, world, host_entity)
         if action is None:
             return Action(action_type=PlayerAction.IDLE)
         return action
 
     def transition_to(self, new_state: BotState, world: WorldInfo, entity: PlayerInfo):
-        # Clean up memory before switching
-        self.memory.set_path([])
-        self.memory.set_goal(None)
-        self.memory.set_strict_movement(False)
+        # Clean up _memory before switching
+        self._memory.set_path([])
+        self._memory.set_goal(None)
+        self._memory.set_strict_movement(False)
 
-        self.current_state = new_state
-        self.current_state.on_enter(self.memory, world, entity)
+        self._current_state = new_state
+        self._current_state.on_enter(self._memory, world, entity)
 
     def _perform_global_reevaluation(self, world: WorldInfo, bot: PlayerInfo):
-        config = self.memory.config
+        config = self._memory.config
 
         # 1 - Danger
-        if self.memory.config.danger_check_type.is_in_danger(world, bot, self.memory.config.danger_radius):
-            if not isinstance(self.current_state, EscapeState):
+        if self._memory.config.danger_check_type.is_in_danger(world, bot, self._memory.config.danger_radius):
+            if not isinstance(self._current_state, EscapeState):
                 self.transition_to(EscapeState(), world, bot)
             return
 
         # 2 - Powerup Check
         if random.random() <= config.powerup_chance:
             target = config.powerup_policy.get_goal(world, bot)
-            path = config.powerup_policy.get_path(world, bot, self.memory)
+            path = config.powerup_policy.get_path(world, bot, self._memory)
             if target and path:
-                if isinstance(self.current_state, GetPowerupState):
-                    if self.memory.goal == target:
+                if isinstance(self._current_state, GetPowerupState):
+                    if self._memory.goal == target:
                         return
 
                 self.transition_to(GetPowerupState(target), world, bot)
@@ -168,103 +186,74 @@ class BotController:
             return
 
         # 4 - Default -> Wander
-        if not isinstance(self.current_state, WanderState):
+        if not isinstance(self._current_state, WanderState):
             self.transition_to(WanderState(), world, bot)
+
+class HostileBotConfig:
+    bot_type = BotType.HOSTILE
+
+    reeval_interval = 0.5
+    reeval_chance = 0.25
+
+    danger_radius = 0
+    danger_check_type = BombOnlyDangerPolicy()
+
+    attack_policy = AttackPolicy2()
+    attack_range_trigger = 2
+    attack_search_radius = 0
+
+    powerup_policy = PowerupPolicy2()
+    powerup_chance = 0.20
+
+class CarefulBotConfig:
+    bot_type = BotType.CAREFUL
+
+    reeval_interval = 0.25
+    reeval_chance = 1.0
+
+    danger_radius = 4
+    danger_check_type = ExplosionPredictionDangerPolicy()
+
+    attack_policy = AttackPolicy1(max_distance=3)
+    attack_range_trigger = 4
+    attack_search_radius = 3
+
+    powerup_policy = PowerupPolicy2()
+    powerup_chance = 1.0
+
+
+class GreedyBotConfig:
+    bot_type = BotType.GREEDY
+
+    reeval_interval = 1.0
+    reeval_chance = 1.0
+
+    danger_radius = 2
+    danger_check_type = ExplosionPredictionDangerPolicy()
+
+    attack_policy = AttackPolicy1(max_distance=6)
+    attack_range_trigger = 3
+    attack_search_radius = 6
+
+    powerup_policy = PowerupPolicy1()
+    powerup_chance = 1.0
 
 
 class BotFactory:
     @classmethod
     def make(cls, bot_type: BotType) -> BotControllerInfo:
         config = cls.create_bot_config(bot_type)
-        memory = BotMemory(config)
-        controller = BotController(memory)
-        return controller
+        _memory = BotMemory(config)
+        return BotController(_memory)
 
     @classmethod
     def create_bot_config(cls, bot_type: BotType) -> BotConfigInfo:
         match bot_type:
             case BotType.HOSTILE:
-                return BotConfig(
-                    # Reevaluation
-                    reeval_interval=0.5,
-                    reeval_chance=0.25,
-
-                    # Danger sensing
-                    danger_radius=0,  # only current cell
-                    danger_check_type=BombOnlyDangerPolicy(),
-
-                    # Attack
-                    attack_policy=AttackPolicy2(),
-                    attack_range_trigger=2,
-                    attack_search_radius=0,  # unused by policy 2
-
-                    # Powerups
-                    powerup_policy=PowerupPolicy2(),
-                    powerup_chance=0.20,
-                )
-
+                return HostileBotConfig()
             case BotType.CAREFUL:
-                return BotConfig(
-                    # Reevaluation
-                    reeval_interval=0.25,
-                    reeval_chance=1.0,
-
-                    # Danger sensing
-                    danger_radius=4,
-                    danger_check_type=ExplosionPredictionDangerPolicy(),
-
-                    # Attack
-                    attack_policy=AttackPolicy1(
-                        max_distance=3,
-                    ),
-                    attack_range_trigger=4,
-                    attack_search_radius=3,
-
-                    # Powerups
-                    powerup_policy=PowerupPolicy2(),
-                    powerup_chance=1.0,
-                )
-
+                return CarefulBotConfig()
             case BotType.GREEDY:
-                return BotConfig(
-                    # Reevaluation
-                    reeval_interval=1.0,
-                    reeval_chance=1.0,
-
-                    # Danger sensing
-                    danger_radius=2,
-                    danger_check_type=ExplosionPredictionDangerPolicy(),
-
-                    # Attack
-                    attack_policy=AttackPolicy1(
-                        max_distance=6,
-                    ),
-                    attack_range_trigger=3,
-                    attack_search_radius=6,
-
-                    # Powerups
-                    powerup_policy=PowerupPolicy1(),
-                    powerup_chance=1.0,
-                )
-
+                return GreedyBotConfig()
             case _:
                 raise ValueError(f"Unknown bot type: {bot_type}")
-
-
-@dataclass(frozen=True)
-class BotConfig:
-    # Reevaluation Settings
-    reeval_interval: float  # Seconds
-    reeval_chance: float    # 0.0 to 1.0 (percent/100)
-
-    # Danger Sensing
-    danger_radius: int      # 'D'
-    danger_check_type: DangerPolicy
-
-    # Policies
-    attack_policy: PathfindingPolicy
-    attack_range_trigger: int  # 'R' (Plant bomb if enemy within this distance)
-    attack_search_radius: int # 'A' (Policy 1: check players within this dist)
-
-    powerup_policy: PathfindingPolicy
-    powerup_chance: float   # 0.0 to 1.0 (Chance to use Policy 2)
