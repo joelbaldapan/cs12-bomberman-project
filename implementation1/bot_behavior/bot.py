f897baeccd9ea8fd7e19a219ccd04b41796ed97f
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-12-30 22:13:06 +0800

    refactor(py): separate bot memory and config

--- implementation1/bot_behavior/bot.py
+++ implementation1/bot_behavior/bot.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from bot_behavior.bot_policy import AttackPolicy1, AttackPolicy2, BombOnlyDangerPolicy, ExplosionPredictionDangerPolicy, PowerupPolicy1, PowerupPolicy2
-from common_types import BotConfigInfo, BotMemoryInfo, BotControllerInfo, BotPlayerInfo, BotState, ActionInfo, Action, PlayerAction, BombInfo, ExplosionInfo, WorldInfo, GridCoords, BotType
+from common_types import BotConfigInfo, BotControllerInfo, BotPlayerInfo, BotState, ActionInfo, Action, PlayerAction, BombInfo, ExplosionInfo, WorldInfo, GridCoords, BotType
 from .bot_state import AttackState, EscapeState, GetPowerupState, WanderState
 import random
 
@@ -8,8 +8,7 @@ import random
 class BotMemory():
     """Memory for the bot."""
 
-    def __init__(self, config: BotConfigInfo):
-        self._config = config
+    def __init__(self):
         self._reeval_timer: float = 0.0
 
         # Navigation Memory
@@ -20,10 +19,6 @@ class BotMemory():
         # Perception _memory
         self._last_bomb_coords: set[GridCoords] = set()
         self._last_explosion_coords: set[GridCoords] = set()
-
-    @property
-    def config(self) -> BotConfigInfo:
-        return self._config
 
     @property
     def reeval_timer(self) -> float:
@@ -67,35 +62,38 @@ class BotMemory():
     def set_last_explosion_coords(self, coords: set[GridCoords]) -> None:
         self._last_explosion_coords = coords
 
-    def tick_reeval(self, dt: float) -> bool:
+    def tick_reeval(self, dt: float, reeval_interval: float, reeval_chance: float) -> bool:
         """Advance the reevaluation timer. Return `True` if reevaluation should occur."""
         self._reeval_timer += dt
 
-        if self._reeval_timer >= self._config.reeval_interval:
+        if self._reeval_timer >= reeval_interval:
             self._reeval_timer = 0.0
-            return random.random() <= self._config.reeval_chance
+            return random.random() <= reeval_chance
 
         return False
 
 
 class BotController:
-    def __init__(self, bot_memory: BotMemoryInfo):
-        self._memory = bot_memory
+    def __init__(self, bot_config: BotConfigInfo):
+        self._config = bot_config
+        self._memory = BotMemory()
         self._current_state: BotState = WanderState()
         self._initialized = False
         self._last_bomb_count = 0
         self._last_explosion_count = 0
 
-    def update(self, dt: float, host_entity: BotPlayerInfo, world: WorldInfo) -> None:
-        # Init state on first frame
-        if not self._initialized:
-            self._current_state.on_enter(self._memory, world, host_entity)
+    def _initialize(self, dt: float, host_entity: BotPlayerInfo, world: WorldInfo):
+            self._current_state.on_enter(self._config, self._memory, world, host_entity)
             self._initialized = True
-            # Track coordinates instead of IDs or Objects
             self._last_bomb_coords = {(b.row, b.col)
                                       for b in world.get_all_type(BombInfo)}
             self._last_explosion_coords = {
                 (e.row, e.col) for e in world.get_all_type(ExplosionInfo)}
+            
+    def update(self, dt: float, host_entity: BotPlayerInfo, world: WorldInfo) -> None:
+        # Init state on first frame
+        if not self._initialized:
+            self._initialize(dt, host_entity, world)
 
         should_force_reeval = False
 
@@ -122,9 +120,9 @@ class BotController:
         self._memory.set_last_explosion_coords(current_explosion_coords)
 
 
-        timer_trigger = self._memory.tick_reeval(dt)
-        is_in_danger = self._memory.config.danger_check_type.is_in_danger(
-            world, host_entity, self._memory.config.danger_radius
+        timer_trigger = self._memory.tick_reeval(dt, self._config.reeval_interval, self._config.reeval_chance)
+        is_in_danger = self._config.danger_check_type.is_in_danger(
+            world, host_entity, self._config.danger_radius
         )
 
         # Check if danger!
@@ -137,14 +135,12 @@ class BotController:
             self._perform_global_reevaluation(world, host_entity)
 
         if self._current_state:
-            next_state = self._current_state.on_tick(
-                self._memory, world, host_entity)
+            next_state = self._current_state.on_tick(self._config, self._memory, world, host_entity)
             if next_state:
                 self.transition_to(next_state, world, host_entity)
 
     def decide_action(self, host_entity: BotPlayerInfo, world: WorldInfo) -> ActionInfo:
-        action = self._current_state.decide_action(
-            self._memory, world, host_entity)
+        action = self._current_state.decide_action(self._config, self._memory, world, host_entity)
         if action is None:
             return Action(action_type=PlayerAction.IDLE)
         return action
@@ -156,21 +152,19 @@ class BotController:
         self._memory.set_strict_movement(False)
 
         self._current_state = new_state
-        self._current_state.on_enter(self._memory, world, entity)
+        self._current_state.on_enter(self._config, self._memory, world, entity)
 
     def _perform_global_reevaluation(self, world: WorldInfo, bot: BotPlayerInfo):
-        config = self._memory.config
-
         # 1 - Danger
-        if self._memory.config.danger_check_type.is_in_danger(world, bot, self._memory.config.danger_radius):
+        if self._config.danger_check_type.is_in_danger(world, bot, self._config.danger_radius):
             if not isinstance(self._current_state, EscapeState):
                 self.transition_to(EscapeState(), world, bot)
             return
 
         # 2 - Powerup Check
-        if random.random() <= config.powerup_chance:
-            target = config.powerup_policy.get_goal(world, bot)
-            path = config.powerup_policy.get_path(world, bot, self._memory)
+        if random.random() <= self._config.powerup_chance:
+            target = self._config.powerup_policy.get_goal(world, bot)
+            path = self._config.powerup_policy.get_path(world, bot, self._memory)
             if target and path:
                 if isinstance(self._current_state, GetPowerupState):
                     if self._memory.goal == target:
@@ -180,7 +174,7 @@ class BotController:
                 return
 
         # 3 - Attack Check
-        target_attack_pos = config.attack_policy.get_goal(world, bot)
+        target_attack_pos = self._config.attack_policy.get_goal(world, bot)
         if target_attack_pos:
             self.transition_to(AttackState(target_attack_pos), world, bot)
             return
@@ -243,8 +237,7 @@ class BotFactory:
     @classmethod
     def make(cls, bot_type: BotType) -> BotControllerInfo:
         config = cls.create_bot_config(bot_type)
-        _memory = BotMemory(config)
-        return BotController(_memory)
+        return BotController(config)
 
     @classmethod
     def create_bot_config(cls, bot_type: BotType) -> BotConfigInfo:
--- implementation1/bot_behavior/bot_state.py
+++ implementation1/bot_behavior/bot_state.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
-from common_types import  Action, ActionInfo, BotPlayerInfo, BotState, PlayerAction, BotMemoryInfo, EntityType, GridCoords, WorldInfo
+from common_types import  Action, ActionInfo, BotConfigInfo, BotPlayerInfo, BotState, PlayerAction, BotMemoryInfo, EntityType, GridCoords, WorldInfo
 from bot_behavior.helpers.pathfinding import follow_path_action, get_random_floor_cell, get_reachable_safe_cell
 from .bot_policy import get_shortest_path, get_manhattan
 
 
 class WanderState:
-    def on_enter(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> None:
+    def on_enter(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> None:
         goal = get_random_floor_cell(world)
         memory.set_goal(goal)
 
@@ -13,23 +13,23 @@ class WanderState:
             world, (entity.row, entity.col), goal, ignore_soft_blocks=True)
         memory.set_path(path)
 
-    def on_tick(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> BotState | None:
+    def on_tick(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> BotState | None:
         if memory.goal and (entity.row, entity.col) == memory.goal:
-            self.on_enter(memory, world, entity)
+            return WanderState()
 
         if not memory.path and memory.goal != (entity.row, entity.col):
-            self.on_enter(memory, world, entity)
+            return WanderState()
 
         return None
 
-    def decide_action(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> ActionInfo | None:
+    def decide_action(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> ActionInfo | None:
         return follow_path_action(memory, world, entity, allow_bombing=True)
 
 
 class EscapeState:
-    def on_enter(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> None:
+    def on_enter(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> None:
         self._left_danger = False
-        dangerous_cells = memory.config.danger_check_type.get_all_danger_zones(
+        dangerous_cells = config.danger_check_type.get_all_danger_zones(
             world)
         goal = get_reachable_safe_cell(world, entity, dangerous_cells)
 
@@ -42,12 +42,12 @@ class EscapeState:
             memory.set_goal(None)
             memory.set_path([])
 
-    def on_tick(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> BotState | None:
+    def on_tick(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> BotState | None:
         overlap_pos = entity.get_overlapping_cells()
-        danger_zones = memory.config.danger_check_type.get_all_danger_zones(world)
+        danger_zones = config.danger_check_type.get_all_danger_zones(world)
 
         if not memory.path:
-            self.on_enter(memory, world, entity)
+            self.on_enter(config, memory, world, entity)
 
         in_danger = any(p in danger_zones for p in overlap_pos)
         if not in_danger:
@@ -57,7 +57,7 @@ class EscapeState:
 
         if self._left_danger:
             if any(p in danger_zones for p in memory.path):
-                self.on_enter(memory, world, entity)
+                self.on_enter(config, memory, world, entity)
 
         # Success check
         if memory.goal and (entity.row, entity.col) == memory.goal:
@@ -70,7 +70,7 @@ class EscapeState:
 
         return None
 
-    def decide_action(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> ActionInfo | None:
+    def decide_action(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> ActionInfo | None:
         return follow_path_action(memory, world, entity, allow_bombing=False)
 
 
@@ -78,18 +78,18 @@ class GetPowerupState:
     def __init__(self, target_goal: GridCoords):
         self._target_goal = target_goal
 
-    def on_enter(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> None:
+    def on_enter(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> None:
         memory.set_goal(self._target_goal)
-        path = memory.config.powerup_policy.get_path(world, entity, memory)
+        path = config.powerup_policy.get_path(world, entity, memory)
         memory.set_path(path)
 
-    def on_tick(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> BotState | None:
+    def on_tick(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> BotState | None:
         if (entity.row, entity.col) == memory.goal:
             return WanderState()
         return None
 
-    def decide_action(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> ActionInfo | None:
-        allow_bomb = memory.config.powerup_policy.can_place_bomb()
+    def decide_action(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> ActionInfo | None:
+        allow_bomb = config.powerup_policy.can_place_bomb()
         return follow_path_action(memory, world, entity, allow_bombing=allow_bomb)
 
 
@@ -97,21 +97,21 @@ class AttackState:
     def __init__(self, target_goal: GridCoords):
         self._target_goal = target_goal
 
-    def on_enter(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> None:
+    def on_enter(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> None:
         memory.set_goal(self._target_goal)
-        path = memory.config.attack_policy.get_path(world, entity, memory)
+        path = config.attack_policy.get_path(world, entity, memory)
         memory.set_path(path)
 
-    def on_tick(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> BotState | None:
+    def on_tick(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> BotState | None:
         if (entity.row, entity.col) == memory.goal:
             return WanderState()
         return None
 
-    def decide_action(self, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> ActionInfo | None:
+    def decide_action(self, config: BotConfigInfo, memory: BotMemoryInfo, world: WorldInfo, entity: BotPlayerInfo) -> ActionInfo | None:
         # Check Attack Range Trigger
         if memory.goal:
             dist = get_manhattan((entity.row, entity.col), memory.goal)
-            if dist <= memory.config.attack_range_trigger:
+            if dist <= config.attack_range_trigger:
                 curr = world.get_entity_at(entity.row, entity.col)
                 has_bomb = curr and curr.entity_type == EntityType.BOMB
                 if not has_bomb:
